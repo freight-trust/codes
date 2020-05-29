482f6204f7e0c6860877db1f14d29d44acbbbd3e
sam <sam@freighttrust.com>
2020-05-29 10:06:00 +0000

    adding gradle build

---
+++ modules/gradle/docs/gradlew.bat
@@ -0,0 +1,8 @@
+@rem
+@rem Credits to  Jez Higgins (@jezhiggins) & Ralph McArdell (@ralph-mcardell)
+@rem for hints on how to have a very simplified batch file.
+@rem
+@echo off
+
+setlocal enableextensions
+%~p0\..\gradlew.bat %*
